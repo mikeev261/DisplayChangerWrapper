d7bf457b702f82453f96138e89895e5bf4fdd749
mikeev261 <mikeev@gmail.com>
2021-08-22 11:35:25 -0400

    Cleaned up variables and added usage info.

--- mon_mode.ps1
+++ mon_mode.ps1
@@ -1,45 +1,64 @@
+#USAGE: .\mon_mode.ps1 <MONITOR CONFIG MODE STRING>
+#EXAMPLE: .\mon_mode.ps1 'L_P' #sets up 2 monitors to be landscape to the left, portrait to the right
+
+#Don't change:
 $mode = $args[0].ToUpper()
 
+#USER SETTINGS/INSTRUCTIONS: 
+#1. Download and install DisplayChanger (download from here: https://12noon.com/?page_id=80)
+#2. Enable all monitors
+#3. Run .\dc64cmd.exe -listmonitors and make note of the names of your monitors
+#4. Based on the names of your monitors as defined in the output of step #3, set them below
+$left_mon = '\\.\DISPLAY2'
+$right_mon = '\\.\DISPLAY1'
+#5. Set Location of your install of DisplayChanger below
 $dc = 'C:\Users\mikeev\Desktop\Tools\DisplayChanger\dc64cmd.exe'
-$left_mon = $dc + ' -monitor="\\.\DISPLAY2"'
-$right_mon = $dc + ' -monitor="\\.\DISPLAY1"'
 
+
+#6. Modify to add more monitors (or rename/reorder)
+$mon1 = $dc + ' -monitor="' + $left_mon + '"'
+$mon2 = $dc + ' -monitor="' + $right_mon + '"'
+
+#7. Define your modes below. The switch string (like 'L_L') is what you'll pass-in through the command-line
+#Make careful note of the -rotate and -lx,-ty rotation and positioning values. 
+#You'll need to play with this via trial and error to get them just right for your setup. 
 
 switch ($mode)
 {
     'L_L' #Landscape + Landscape (Extend)
     {
-        $UWmon = $left_mon + ' -primary -more'
-        $4Kmon = $right_mon + ' -secondary -rotate=up -lx=3840 -ty=-256 -apply'
+        $UWmon = $mon1 + ' -primary -more'
+        $4Kmon = $mon2 + ' -secondary -rotate=up -lx=3840 -ty=-256 -apply'
         
     }
     'L_P' #Landscape + Portrait (Extend)
     {
-        $UWmon = $dc + ' -monitor="\\.\DISPLAY2" -primary -more'
-        $4Kmon = $dc + ' -monitor="\\.\DISPLAY1" -secondary -rotate=left -lx=3840 -ty=-1500 -apply'
+        $UWmon = $mon1 + ' -primary -more'
+        $4Kmon = $mon2 + ' -monitor="\\.\DISPLAY1" -secondary -rotate=left -lx=3840 -ty=-1500 -apply'
         
     }
     'L_OFF' #Landscape + OFF
     {
-        $UWmon = $dc + ' -monitor="\\.\DISPLAY2" -primary -more'
-        $4Kmon = $dc + ' -monitor="\\.\DISPLAY1" -detach -apply'
+        $UWmon = $mon1 + ' -primary -more'
+        $4Kmon = $mon2 + ' -monitor="\\.\DISPLAY1" -detach -apply'
         
     }
     'OFF_L' #OFF + Landscape
     {
-        $UWmon = $dc + ' -monitor="\\.\DISPLAY2" -detach -more'
-        $4Kmon = $dc + ' -monitor="\\.\DISPLAY1" -primary -rotate=up -apply'
+        $UWmon = $mon1 + ' -detach -more'
+        $4Kmon = $mon2 + ' -monitor="\\.\DISPLAY1" -primary -rotate=up -apply'
         
     }
     'OFF_P' #OFF + Portrait
     {
-        $UWmon = $dc + ' -monitor="\\.\DISPLAY2" -detach -more'
-        $4Kmon = $dc + ' -monitor="\\.\DISPLAY1" -primary -rotate=left -apply'
+        $UWmon = $mon1 + ' -detach -more'
+        $4Kmon = $mon2 + ' -monitor="\\.\DISPLAY1" -primary -rotate=left -apply'
         
     }
 }
 
-
+#8. Modify to add more monitors. 
+#IMPORTANT: all initial monitors should have -more, and the final monitor should always have -apply
 Write-Output $UWmon
 Invoke-Expression $UWmon
 Write-Output $4Kmon
